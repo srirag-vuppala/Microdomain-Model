c4501aa6e94fab5996d1068e3f63c2ecfdf925ef
srirag-vuppala <srirag.vuppala@gmail.com>
2021-08-04 21:32:30 -0700

    add in changes for 83 and laplacian

--- model.py
+++ model.py
@@ -14,13 +14,13 @@ import hh
 np.set_printoptions(precision=3)
 
 
-def create_strand(n_cells):
+def create_strand(n_cells, phi_resting):
     # Creating a strand representing n cells
     # The extracellular space is initially at a potential of   0 mV 
     # The strand is [phi_i phi_e]
     strand = np.zeros(n_cells * 2)
     # The intracellular space is initially at a potential of -70 mV
-    strand[:n_cells] += -70
+    strand[:n_cells] += phi_resting 
     return strand 
 
 
@@ -31,89 +31,35 @@ def generate_ionic_current(V, A, delta_t):
     return 1000 * delta_t * np.asarray(I_ion)
 
 
-def generate_A_matrix(n_cells, delta_x):
-    # Creates "A" matrix from Microdomain Coding Prep
-    # "A" matrix approximates second derivative of phi intra, A is n_cells x 2*n_cells
-    A = np.zeros([n_cells, n_cells])
-    A += np.diagflat(np.ones(n_cells) * -2)  # add main diagonal of -2's
-    A += np.diagflat(np.ones(n_cells - 1), 1)  # add super diagonal of 1's
-    A += np.diagflat(np.ones(n_cells - 1), -1)  # add sub diagonal of 1's
-    zero = np.zeros([n_cells, n_cells])  # create zero matrix to be appended to A
-    A = np.concatenate((A, zero), axis=1)  # horizontally merge A and zero
-    A *= (1 / delta_x ** 2)  # multiply A by 1/delta_x^2
-    return A
-
-
-def generate_B_matrix(n_cells):
-    # Creates "B" matrix from Microdomain Coding Prep
-    # "B" matrix transforms phi_n into transmembrane potential, B is n_cells x 2*n_cells
-    B = np.eye(n_cells)
-    B_merge = np.zeros([n_cells, n_cells])
-    B_merge += np.diagflat(np.ones(n_cells) * -1)  # created -I component of B this way to avoid -0's
-    B = np.concatenate((B, B_merge), axis=1)  # horizontally merge B and B_merge
-    return B
-
-
-def generate_E_matrix(n_cells):
-    # creates "E" matrix from Microdomain coding prep
-    # "E" matrix transforms phi_n into phi_i - phi_neighbors, E is n_cells x 2*n_cells
-    main_diag = np.ones(n_cells) * 2  # create main diagonal of 2's
-    main_diag[0] = 1  # change first entry to 1
-    main_diag[-1] = 1  #  change last entry to 1
-    # first and last entries changed to 1 to account for boundary cases
-    E = np.zeros([n_cells, n_cells])
-    E += np.diagflat(main_diag)  # add main_diag to diagonal of E
-    E += np.diagflat(np.ones(n_cells - 1) * -1, 1)  # create super diagonal of -1's
-    E += np.diagflat(np.ones(n_cells - 1) * -1, -1)  # create sub diagonal of -1's
-    zero = np.zeros([n_cells, n_cells])
-    E = np.concatenate((E, zero), axis=1)  # horizontally concatenate E and zero matrix
-    return E
-
-def generate_Z_matrix(n_cells, delta_x):
-    # Creates "Z" matrix from Microdomain Coding Prep
-    # "Z" matrix approximates third derivative of phi extra, Z is n_cells x 2*n_cells
-    Z = np.zeros([n_cells, n_cells])
-    Z += np.diagflat(np.ones(n_cells - 1) * -2, 1)  # create super diagonal of -2's
-    Z += np.diagflat(np.ones(n_cells - 1) * 2, -1)  # create sub diagonal of 2's
-    Z += np.diagflat(np.ones(n_cells - 2), 2)  # create diagonal above super diagonal of 1's
-    Z += np.diagflat(np.ones(n_cells - 2) * -1, -2)  # create diagonal below sub diagonal of -1's
-    zero = np.zeros([n_cells, n_cells])
-    Z = np.concatenate((zero, Z), axis=1)  # horizontally concatenate zero matrix and Z
-    Z *= 1/(2*delta_x**3)
-    return Z
-
-def simulate(A, B, strand, S ):
+def simulate(A, B, strand, n_cells):
     phi_now = strand
+    identity_B = create_B(n_cells)
     for i in range(1000):
         # stimulus
         if i < 5:
             phi_now[0] = -40
             phi_now[-1] = -40
         left_term = np.matmul(B, phi_now)
-        right_term = generate_ionic_current(phi_now, ...)/S
+        right_term = generate_ionic_current(phi_now, ...)
         soln_term = left_term + right_term
         phi_next = np.linalg.solve(A, soln_term)
 
         # set up for next iteration 
         phi_now = phi_next
 
-
-
-
-
 def main():
     # Prep work 
     # Create the strand first
     n_cells = 10
-    strand = create_strand(n_cells)
     delta_x = 0.01
     delta_t = 0.01
     J = 0.1
     S = 0.1
-    v_resting = -70
+    phi_resting = -70
+    strand = create_strand(n_cells, phi_resting)
 
     #TODO : make sure the integral is proper
-    integral = generate_integral(v_resting)
+    integral = generate_integral(phi_resting)
 
     A_84 = create_A_84(delta_x, n_cells, J, S, delta_t)
     B_84 = create_B_84(delta_x, n_cells, J, S, delta_t)
@@ -123,11 +69,8 @@ def main():
     A_comb = np.concatenate((A_83, A_84))
     B_comb = np.concatenate((B_83, B_84))
 
-    """Shouldn't need to stack two strands, that makes our vector [phi_i phi_e phi_i phi_e]"""
-    #combined_strand = np.concatenate((strand, strand))
-
     # Simulate
-    simulate(A_comb, B_comb, strand, S)
+    simulate(A_comb, B_comb, strand, n_cells)
 
     # os.system("ffmpeg -y -i 'foo%03d.jpg' bidomain.mp4")
     # os.system("rm -f *.jpg")
--- transformMatrices.py
+++ transformMatrices.py
@@ -1,47 +1,40 @@
 import numpy as np
+from utilities import *
 
 
 def create_A_84(delta_x, n_nodes, J, S, delta_t):
     Z = create_Z(delta_x, n_nodes)
     B = create_B(n_nodes)
-    return (J/S) * Z - (1/delta_t) * B
+    return (J*Z) - (S*B/delta_t) 
 
 def create_B_84(delta_x, n_nodes, J, S, delta_t):
     Z = create_Z(delta_x, n_nodes)
     B = create_B(n_nodes)
-    return (-1/delta_t) * B - (J/S) * Z
+    return (-S*B/delta_t) - (J*Z)
 
 def create_A_83(delta_x, n_nodes, S, delta_t, integral):
     """ The coeff matrix of the LHS in 83"""
-    A = create_A(delta_x, n_nodes)
-    lhs = A/(2*S)
-    B = create_B(n_nodes)
-    rhs = B/delta_t
-    C = create_C(n_nodes)
-    G = integral * C
-    return lhs - rhs - G/(2*S)
+    term_1 = create_A(delta_x, n_nodes)/2
+    term_2 = S*create_B(n_nodes)/delta_t
+    term_3 = create_G(n_nodes, integral)/2
+    return term_1 - term_2 - term_3
 
 def create_B_83(delta_x, n_nodes, S, delta_t , integral):
     """ The coeff matrix of the RHS in 83"""
-    B = create_B(n_nodes)
-    term_1 = B/delta_t
-    A = create_A(delta_x, n_nodes)
-    term_2 = A/(2*S)
-    C = create_C(n_nodes)
-    term_3 = (integral*C)/(2*S)
+    term_1 = create_A(delta_x, n_nodes)/2
+    term_2 = S*create_B(n_nodes)/delta_t
+    term_3 = create_G(n_nodes, integral)/2
     return term_3 - term_1 - term_2
 
 def create_Z(delta_x, n_nodes):
     L_3 = create_L_3(delta_x, n_nodes)
     zero = np.zeros([n_nodes, n_nodes])
-    '''matrix should look like [ 0 L3], pretty sure its incorrect in the coding prep since it should be third 
-    derivative of phi_e '''
     return np.concatenate((zero, L_3), axis=1)
 
-def create_C(n_nodes):
+def create_G(n_nodes, integral):
     E = create_E(n_nodes)
     zero = np.zeros([n_nodes, n_nodes])
-    return np.concatenate((E, zero), axis=1)
+    return integral*np.concatenate((E, zero), axis=1)
 
 def create_E(n_nodes):
     E = np.diagflat([2 for _ in range(n_nodes)], 0) + np.diagflat([-1 for _ in range(n_nodes-1)], -1) + np.diagflat([-1 for _ in range(n_nodes-1)], 1)
@@ -58,7 +51,6 @@ def create_A(delta_x, n_nodes):
 
 def create_B(n_nodes):
     """ B = [I -I] """
-    '''second identity matrix should be negative'''
     return np.concatenate((np.identity(n_nodes), np.negative(np.identity(n_nodes))), axis=1)
     
 
@@ -66,11 +58,11 @@ def create_L_2(delta_x, n_nodes ):
     """ Create a laplacian matrix for the 3 point stencil that corresponds to equation 83"""
     constant = 1/(delta_x**2)
 
-    L = np.diagflat([constant for _ in range(n_nodes-1)], -1) + np.diagflat([-2*constant for _ in range(n_nodes)]) + np.diagflat([constant for _ in range(n_nodes-1)], 1)
+    L = np.diagflat([constant for _ in range(n_nodes-1)], -1) + np.diagflat([-2*constant for _ in range(n_nodes)], 0) + np.diagflat([constant for _ in range(n_nodes-1)], 1)
     # Defining the boundary conditions
     # TODO Make sure this is right
-    L[0][0] = 1+constant
-    L[-1][-1] = 1+constant
+    L[0][0] = -constant
+    L[-1][-1] = -constant
     return L
 
 def create_L_3(delta_x, n_nodes):
@@ -80,16 +72,32 @@ def create_L_3(delta_x, n_nodes):
     L = np.diagflat([-1*constant for _ in range(n_nodes-2)],-2) + np.diagflat([constant*2 for _ in range(n_nodes-1)], -1) + np.diagflat([-2*constant for _ in range(n_nodes-1)], 1) + np.diagflat([constant for _ in range(n_nodes-2)], 2)
 
     # Defining the boundary conditions
-    # TODO Make sure this is right | WHAT IS THIS
-    L[0][0] = 1+constant
-    L[-1][-1] = 1+constant
+    # TODO Make sure this is right 
+    # Maybe boundary conditions are unnecessary since the laplacian already fits our checks?
+
+    # L[0][0] = constant
+    # L[-1][-1] = constant
     return L
 
+def check_laplace_matrix(L):
+    # I might be checking this wrong
+    # Each row's elements sum should be = 0
+    # Each column's elements sum should be = 0
+    # Thus total sum of all elements should be 0 too 
+    tsum = 0
+    for arr in L:
+        for i in arr:
+            tsum += i
+    if tsum == 0:
+        print("yay it works")    
+    else:
+        print("Try again")
+
 def main():
-    print(create_E(5))
-    print(create_C(5))
-
-
-
+    L2 = create_L_2(0.01, 10)
+    L3 = create_L_3(0.01, 10)
+    matprint(L2)
+    matprint(L3)
+    
 if __name__ == '__main__':
     main()
