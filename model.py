fbb88f0d4f3a4d622fa1c55214c36fef59ee07f9
srirag-vuppala <srirag.vuppala@gmail.com>
2021-08-22 17:25:26 -0700

    add comments with model

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ from integral import *
 from utilities import *
 from transformMatrices import *
 import os
-import hh
+# import hh
 import matplotlib.pyplot as plt
 from ionic_current_cubic import *
 
@@ -26,43 +26,37 @@ def create_strand(n_cells, phi_resting):
     return strand 
 
 
-def generate_ionic_current(V, A, delta_t):
-    V_send = np.matmul(A, V)
-    V_send = np.add(V_send, 70)
-    I_ion = hh.HodgkinHuxley().main(flat(V_send))
-    return 1000 * delta_t * np.asarray(I_ion)
-
-def generate_ionic_dummy(v):
-    return np.asarray(hh.HodgkinHuxley().main(v))
-
 
 def simulate(A, B, strand, n_cells, delta_v, phi_resting, a):
     phi_now = strand
-    transmembrane_transform = create_B(n_cells)
 
     # row control to make A full rank
-    replacement_row = np.ones(n_cells*2)
-    replacement_row[n_cells+1:] = 0
-    replacement_row_2 = np.ones(n_cells*2)
-    replacement_row_2[:n_cells+1] = 0
+    # replacement_row = np.ones(n_cells*2)
+    # replacement_row[n_cells+1:] = 0
+    # replacement_row_2 = np.ones(n_cells*2)
+    # replacement_row_2[:n_cells+1] = 0
     # A[0] = replacement_row
-    #A[-1] = replacement_row_2
-    #print(replacement_row)
+    # A[-1] = replacement_row_2
 
-    print("Rank of A: " + str(np.linalg.matrix_rank(A)))
+    # print("Rank of A: " + str(np.linalg.matrix_rank(A)))
 
+    # setting an arbitrary number of 1000 
+    # can be tweaked if wanted to run for longer or shorter 
     for i in range(1000):
+
+        intra, extra = split_list(phi_now) # split the strand to make sure we are modifying intra and extra properly
+
         # stimulus
-        intra, extra = split_list(phi_now)
+        # Setting a potential difference by setting the extremeties(first and last points) of the strand to opposite voltages
         if i < 5:
             extra[0] += -100
             extra[-1] += 100
         
         phi_trans = intra - extra
         phi_now = join_list(intra, extra)
+
         if i < 200 or i % 100 == 0 or i == 999:
             # plt.scatter(np.arange(1, 11, 1), np.matmul(create_B(n_cells), phi_now))
-            # phi_trans = np.matmul(transmembrane_transform, phi_now)
             plt.plot(np.arange(1, n_cells+1, 1), phi_trans)
             plt.xlabel('Node')
             plt.ylabel('Transmembrane Potential')
@@ -71,16 +65,18 @@ def simulate(A, B, strand, n_cells, delta_v, phi_resting, a):
             plt.clf()
             print("i: " + str(i))
             print(phi_trans)
+
+        # check handwritten doc for variable naming
         RHS_left_term = np.matmul(B, phi_now)
         # if i == 0:
             # phi_trans[-1] = 1 | setting a control to prevent blowup ?
             # RHS_left_term[n_cells-1] = 1
             # RHS_left_term[-1] = 1
             # A[-1] = 1
-        #right_term = generate_ionic_current(phi_now, ...)
-        #right_term = generate_ionic_dummy(phi_now*delta_v)
-        #right_term = generate_cubic_integral(phi_resting, a)
-        #soln_term = RHS_left_term + right_term
+        #RHS_right_term = generate_ionic_current(phi_now, ...)
+        #RHS_right_term = generate_ionic_dummy(phi_now*delta_v)
+        #RHS_right_term = generate_cubic_integral(phi_resting, a)
+        #RHS_soln_term = RHS_left_term + RHS_right_term
         soln_term = RHS_left_term
         # soln_term[0] = 1
         #soln_term[-1] = 1
@@ -96,7 +92,7 @@ def main():
     a = .5
     delta_v = 120
     phi_resting = -70/delta_v
-    n_cells = 100
+    n_cells = 5
     delta_x = 0.1
     delta_t = 1
     J = -3.62*(10**-5) 
@@ -117,16 +113,17 @@ def main():
 
     A_comb = np.concatenate((A_83, A_84))
     A_comb = np.where(A_comb == -0,0, A_comb )
-    B_comb = np.concatenate((B_83, B_84))
+    B_comb = np.concatenate((B_83, B_84), axis=1)
 
-    # A_comb = np.identity(2*n_cells) # the negative sign causes oscillation?
+    # A_comb = np.identity(2*n_cells) 
 
     matprint(A_comb)
     matprint(B_comb)
     # Simulate
-    simulate(A_comb, B_comb, strand, n_cells, delta_v, phi_resting, a)
+    # simulate(A_comb, B_comb, strand, n_cells, delta_v, phi_resting, a)
 
     os.system("ffmpeg -y -i 'timestep%d.jpg' microdomain.mp4")
+    # to delete the images run this command "rm -f *.jpg" in the terminal. Make sure you are in the right directory/folder.
     # os.system("rm -f *.jpg")
 
 
